85ed2f1b3a0e2e79e0c2ba11a4906ab2b1d2cca0
Pranjal Sachan <146807790+pranjalsachan9935@users.noreply.github.com>
2025-05-10 15:09:07 +0530

    Update LoginPage.jsx

--- Frontend/src/pages/LoginPage.jsx
+++ Frontend/src/pages/LoginPage.jsx
@@ -14,7 +14,7 @@ export default function LoginPage() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:4000/user/login", {
+      const response = await fetch("https://job-application-mern-bv2m.vercel.app/user/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
